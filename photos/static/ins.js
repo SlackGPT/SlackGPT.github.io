8a2bcdf7eb196c084006d49f4e626b07aa43062c
rtq <54522645+SlackGPT@users.noreply.github.com>
2023-05-03 22:26:07 +0800

    Update ins.js

--- photos/static/ins.js
+++ photos/static/ins.js
@@ -1,3 +1,4 @@
+
 /******/
 (function(modules) { // webpackBootstrap
   /******/ // The module cache
@@ -111,30 +112,43 @@
       }
     };
     var render = function render(res) {
+      // var ulTmpl = "";
+      // for (var j = 0, len2 = res.list.length; j < len2; j++) {
+        // var data = res.list[j].arr;
+        // var liTmpl = "";
+      //   for (var i = 0, len = data.link.length; i < len; i++) {
+      //     var minSrc = 'http://rrcbaw9sz.hd-bkt.clouddn.com/blog/photos/min_photos/' + data.link[i];
+      //     var src = 'http://rrcbaw9sz.hd-bkt.clouddn.com/blog/photos/photos/' + data.link[i];
+      //     var type = data.type[i];
+      //     var target = src + (type === 'video' ? '.mp4' : '.jpg');
+      //     src += '';
+
+      //     liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
+      //           <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
+      //             <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/photos/static/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+      //           </a>\
+      //           <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
+      //       </figure>';
+      //   }
+        // ulTmpl = ulTmpl + '<section class="album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
+        // <ul class="img-box-ul">' + liTmpl + '</ul>\
+        // </section>';
+      // }
+
+      var liTmpl = "";
+      var src = 'https://w.wallhaven.cc/full/gp/wallhaven-gpq6e7.jpg';
+      var minSrc = 'https://wallhaven.cc/w/gpq6e7';
+      liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
+                  <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + 'image' + '" data-target="' + src + '">\
+                    <img class="reward-img" data-type="' + 'image' + '" data-src="' + minSrc + '" src="/photos/static/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+                  </a>\
+                  <figcaption style="display:none" itemprop="caption description">' + 'Eva Reber' + '</figcaption>\
+                 </figure>';
+
       var ulTmpl = "";
-      for (var j = 0, len2 = res.list.length; j < len2; j++) {
-        var data = res.list[j].arr;
-        var liTmpl = "";
-        for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://rrcbaw9sz.hd-bkt.clouddn.com/blog/photos/min_photos/' + data.link[i];
-          var src = 'http://rrcbaw9sz.hd-bkt.clouddn.com/blog/photos/photos/' + data.link[i];
-          var type = data.type[i];
-          var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          src += '';
-
-          liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
-                <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/photos/static/empty.png" itemprop="thumbnail" onload="lzld(this)">\
-                </a>\
-                <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
-            </figure>';
-        }
-        ulTmpl = ulTmpl + '<section class="album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
-        <ul class="img-box-ul">' + liTmpl + '</ul>\
-        </section>';
-      }
-
-
+      ulTmpl = ulTmpl + '<section class="album"><h1 class="year">' + 2023 + '年<em>' + 5 + '月</em></h1>\
+                          <ul class="img-box-ul">' + liTmpl + '</ul>\
+                         </section>';
       document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="" itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
       createVideoIncon();
       _view2.default.init();
