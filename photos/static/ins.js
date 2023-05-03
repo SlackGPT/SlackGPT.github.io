8b3cd333c7460da03865cf5b4dbda592c0656871
rtq <54522645+SlackGPT@users.noreply.github.com>
2023-05-03 19:23:53 +0800

    Update ins.js

--- photos/static/ins.js
+++ photos/static/ins.js
@@ -116,24 +116,24 @@
       for (var j = 0, len2 = res.list.length; j < len2; j++) {
         var data = res.list[j].arr;
         var liTmpl = "";
-        for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://rrcbaw9sz.hd-bkt.clouddn.com/blog/photos/min_photos/' + data.link[i];
-          var src = 'http://rrcbaw9sz.hd-bkt.clouddn.com/blog/photos/photos/' + data.link[i];
-          var type = data.type[i];
-          var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          src += '';
+      //   for (var i = 0, len = data.link.length; i < len; i++) {
+      //     var minSrc = 'http://rrcbaw9sz.hd-bkt.clouddn.com/blog/photos/min_photos/' + data.link[i];
+      //     var src = 'http://rrcbaw9sz.hd-bkt.clouddn.com/blog/photos/photos/' + data.link[i];
+      //     var type = data.type[i];
+      //     var target = src + (type === 'video' ? '.mp4' : '.jpg');
+      //     src += '';
 
-          liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
-                <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                  <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/photos/static/empty.png" itemprop="thumbnail" onload="lzld(this)">\
-                </a>\
-                <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
-            </figure>';
-        }
-        ulTmpl = ulTmpl + '<section class="album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
-        <ul class="img-box-ul">' + liTmpl + '</ul>\
-        </section>';
-      }
+      //     liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
+      //           <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
+      //             <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/photos/static/empty.png" itemprop="thumbnail" onload="lzld(this)">\
+      //           </a>\
+      //           <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
+      //       </figure>';
+      //   }
+      //   ulTmpl = ulTmpl + '<section class="album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
+      //   <ul class="img-box-ul">' + liTmpl + '</ul>\
+      //   </section>';
+      // }
       document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="" itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
       createVideoIncon();
       _view2.default.init();
